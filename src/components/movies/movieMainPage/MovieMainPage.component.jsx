1afee0187897a67cef73a9bf47a5495a4f6b6d07
Jbaik <joungwoo.baik@gmail.com>
2020-02-28 02:33:10 -0800

    Add loading for MovieMainPage

--- src/components/movies/movieMainPage/MovieMainPage.component.jsx
+++ src/components/movies/movieMainPage/MovieMainPage.component.jsx
@@ -1,57 +1,64 @@
-import React, { useEffect } from 'react'
-import { css } from '@emotion/core'
+import React, { useEffect } from "react"
+import { css } from "@emotion/core"
 
-import ClipLoader from 'react-spinners/ClipLoader'
-import './MovieMainPage.styles.scss'
+import ClipLoader from "react-spinners/ClipLoader"
+import "./MovieMainPage.styles.scss"
 
-import { connect } from 'react-redux'
-import { fetchMovieList } from '../../../redux/movieRedux/movieActions'
-import { fetchTvList } from '../../../redux/tvRedux/tvActions'
+import { connect } from "react-redux"
+import { fetchMovieList } from "../../../redux/movieRedux/movieActions"
+import { fetchTvList } from "../../../redux/tvRedux/tvActions"
 
-import MovieCategory from '../movieCategory/MovieCategory.component'
-import { Container } from 'react-bootstrap'
-
+import MovieCategory from "../movieCategory/MovieCategory.component"
+import { Container } from "react-bootstrap"
 
 //LOADING SPINNER
 const override = css`
   display: block;
   margin: 0 auto;
   border-color: red;
-`;
+`
 
-const MovieMainPage = ({ fetchMovieList, fetchTvList }) => {
-
-    useEffect(() => {
-        fetchMovieList()
-        fetchTvList()
-        // eslint-disable-next-line 
-    }, [])
+const MovieMainPage = props => {
+  useEffect(() => {
+    props.fetchMovieList()
+    props.fetchTvList()
+    // eslint-disable-next-line
+  }, [])
+  if (props.loading) {
     return (
-        <div className='Movie-main-page'>
-            <Container>
-                <MovieCategory categoryName='On TV' />
-                <MovieCategory categoryName='In Theaters' />
-            </Container>
-            <ClipLoader
-                css={override}
-                size={150}
-                //size={"150px"} this also works
-                color={"#123abc"}
-                loading={true}
-            />
-        </div>
+      <ClipLoader
+        css={override}
+        size={150}
+        //size={"150px"} this also works
+        color={"#123abc"}
+        loading={true}
+      />
     )
+  }
+  return (
+    <div className="Movie-main-page">
+      <Container>
+        <MovieCategory categoryName="On TV" />
+        <MovieCategory categoryName="In Theaters" />
+      </Container>
+    </div>
+  )
 }
 
-
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchMovieList: () => { dispatch(fetchMovieList()) },
-        fetchTvList: () => { dispatch(fetchTvList()) }
+const mapStateToProps = state => {
+  return {
+    loading: !(state.movies.loading && state.tvs.loading)
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchMovieList: () => {
+      dispatch(fetchMovieList())
+    },
+    fetchTvList: () => {
+      dispatch(fetchTvList())
     }
+  }
 }
 
-
-
-export default connect(null, mapDispatchToProps)(MovieMainPage)
+export default connect(mapStateToProps, mapDispatchToProps)(MovieMainPage)
