6d9db100eb17f35ae45e7801c8cfe020b3a84985
Jbaik <joungwoo.baik@gmail.com>
2020-03-02 14:48:27 -0800

    Add spinner for person detail page

--- src/components/people/PeopleDetailPage/PeopleDetailPage.component.jsx
+++ src/components/people/PeopleDetailPage/PeopleDetailPage.component.jsx
@@ -6,16 +6,41 @@ import {
   fetchPersonDetail,
   fetchPersonCredit
 } from "../../../redux/peopleRedux/peopleActions"
+import { css } from "@emotion/core"
 
 import PeopleDetailPageLeft from "./peopleDetailPageLeft/PeopleDetailPageLeft.component"
 import PeopleDetailPageRight from "./peopleDetailPageRight/PeopleDetailPageRight.component"
+import { SyncLoader } from "react-spinners"
+
+//LOADING SPINNER
+const override = css`
+  display: block;
+  margin: 0 auto;
+  height: 85vh;
+  text-align: center;
+  padding-top: 40vh;
+  background-color: lightyellow;
+`
 
 const PeopleDetailPage = props => {
-  console.log("SEXSSUKANG", props)
+  console.log("우오오 tv", props)
   useEffect(() => {
     props.fetchPersonDetail()
     props.fetchPesonCredits()
   }, [])
+
+  if (props.loading) {
+    return (
+      <SyncLoader
+        css={override}
+        size={20}
+        //size={"150px"} this also works
+        color={"#3cb"}
+        loading={true}
+        margin={5}
+      />
+    )
+  }
 
   return (
     <div className="people-detail-page">
@@ -30,10 +55,11 @@ const PeopleDetailPage = props => {
 }
 
 const mapStateToProps = state => {
-  console.log("Map State To Props- Detail Page", state)
+  console.log("우오오 Map State To Props- Detail Page", state)
   return {
     person: state.person.person,
-    personCredit: state.person.personCredit
+    personCredit: state.person.personCredit,
+    loading: state.person.loading
   }
 }
 
