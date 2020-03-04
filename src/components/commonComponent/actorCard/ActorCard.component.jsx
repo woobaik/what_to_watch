6f9d8fa15c111253da9a4c67b6b9b58694626328
Jbaik <joungwoo.baik@gmail.com>
2020-03-03 16:49:07 -0800

    Added person link to movie detail page

--- src/components/commonComponent/actorCard/ActorCard.component.jsx
+++ src/components/commonComponent/actorCard/ActorCard.component.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
-import './ActorCard.styles.scss'
+import React from "react"
+import "./ActorCard.styles.scss"
+import { useHistory } from "react-router-dom"
+import { MdPerson } from "react-icons/md"
 
-import { MdPerson } from 'react-icons/md'
+const ActorCard = props => {
+  // Navigate to person detail
+  let history = useHistory()
+  const navigateToPersonDetail = () => {
+    console.log("AC", props)
+    let personUrl = props.id + "-" + props.name.replace(/[\W]/g, "-")
+    console.log(personUrl)
+    history.push(`/people/${personUrl}`)
+  }
 
-const ActorCard = (props) => {
-
-    return (
-        <div className='ActorCard'>
-            {/* w138 h175 */}
-            {props.profile_path ?
-                <img src={`https://image.tmdb.org/t/p/w138_and_h175_face${props.profile_path}`} alt="actor" /> :
-                <div className='no-image-person'>
-                    <MdPerson />
-                </div>}
-            <div className='actor-card-description'>
-                <p className='actor-card-name'>{props.name}</p>
-                <p className='actor-card-role'>{props.character}</p>
-            </div>
+  return (
+    <div className="ActorCard" onClick={navigateToPersonDetail}>
+      {/* w138 h175 */}
+      {props.profile_path ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w138_and_h175_face${props.profile_path}`}
+          alt="actor"
+        />
+      ) : (
+        <div className="no-image-person">
+          <MdPerson />
         </div>
-    )
+      )}
+      <div className="actor-card-description">
+        <p className="actor-card-name">{props.name}</p>
+        <p className="actor-card-role">{props.character}</p>
+      </div>
+    </div>
+  )
 }
 
 export default ActorCard
--- src/components/movies/movieDetailPage/movieDetailInfo/movieDetailInfoLeft/MovieDetailInfoLeft.component.jsx
+++ src/components/movies/movieDetailPage/movieDetailInfo/movieDetailInfoLeft/MovieDetailInfoLeft.component.jsx
@@ -1,43 +1,51 @@
-import React from 'react'
-import {withRouter} from 'react-router-dom'
-import './MovieDetailInfoLeft.styles.scss'
-import { connect } from 'react-redux'
-import ActorCard from '../../../../commonComponent/actorCard/ActorCard.component'
-import HorizontailDivider from '../../../../layouts/divider/HorizontalDivider.component'
+import React from "react"
+import { withRouter } from "react-router-dom"
+import "./MovieDetailInfoLeft.styles.scss"
+import { connect } from "react-redux"
+import ActorCard from "../../../../commonComponent/actorCard/ActorCard.component"
+import HorizontailDivider from "../../../../layouts/divider/HorizontalDivider.component"
 
-const MovieDetailInfoLeft = (props) => {
-   
+const MovieDetailInfoLeft = props => {
+  console.log("PROPS AT CAST ", props)
 
-
-    return (
-        <div className='movieDetailInfoLeft'>
-            <div className="top-billed-cast">
-                <p className='section-title'>Top Billed Cast</p>
-                <div className='actor-card-container'>
-                    {props.topCast ? props.topCast.filter(
-                        (cast, index) => index < 5).map((cast) => {
-                            return <ActorCard key={cast.cast_id ? cast.cast_id : cast.credit_id} name={cast.name} profile_path={cast.profile_path} character={cast.character} />
-                        }) : 'Loading...'}
-                </div>
-                <p className='full-cast-link'>Full Cast & Crew</p>
-                <HorizontailDivider />
-            </div>
-            <div className="media-container"></div>
+  return (
+    <div className="movieDetailInfoLeft">
+      <div className="top-billed-cast">
+        <p className="section-title">Top Billed Cast</p>
+        <div className="actor-card-container">
+          {props.topCast
+            ? props.topCast
+                .filter((cast, index) => index < 5)
+                .map(cast => {
+                  return (
+                    <ActorCard
+                      key={cast.cast_id ? cast.cast_id : cast.credit_id}
+                      id={cast.id}
+                      name={cast.name}
+                      profile_path={cast.profile_path}
+                      character={cast.character}
+                    />
+                  )
+                })
+            : "Loading..."}
         </div>
-    )
+        <p className="full-cast-link">Full Cast & Crew</p>
+        <HorizontailDivider />
+      </div>
+      <div className="media-container"></div>
+    </div>
+  )
 }
 
-
-const mapStateToProps = (state, ownProps) =>   {
-
-    if (ownProps.match.url.slice(0,3) === '/tv') {
-        return {
-            topCast: state.tvs.cast
-        }
-    }
-
+const mapStateToProps = (state, ownProps) => {
+  if (ownProps.match.url.slice(0, 3) === "/tv") {
     return {
-        topCast: state.movies.cast.cast
+      topCast: state.tvs.cast
     }
+  }
+
+  return {
+    topCast: state.movies.cast.cast
+  }
 }
 export default withRouter(connect(mapStateToProps)(MovieDetailInfoLeft))
