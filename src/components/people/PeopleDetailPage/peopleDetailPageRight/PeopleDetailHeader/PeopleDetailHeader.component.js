61e95da93b4da28de140e3c4800d74a379153644
Jbaik <joungwoo.baik@gmail.com>
2020-03-17 12:27:41 -0700

    Add more stying and readmore function for long description

--- src/components/people/PeopleDetailPage/peopleDetailPageRight/PeopleDetailHeader/PeopleDetailHeader.component.js
+++ src/components/people/PeopleDetailPage/peopleDetailPageRight/PeopleDetailHeader/PeopleDetailHeader.component.js
@@ -1,7 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import "./PeopleDetailHeader.style.scss"
 import { connect } from "react-redux"
 const PeopleDetailHeader = ({ person }) => {
+  const [readMore, setReadMore] = useState(false)
+
+  // if person biography is longer than 600, truncate this biography
   const biographyTruncate = str => {
     if (str.length > 600) {
       return str.slice(0, 600) + "..."
@@ -16,13 +19,17 @@ const PeopleDetailHeader = ({ person }) => {
       <div className="people-detail-right-header-biography">
         <p className="people-detail-right-biography-text">Biography</p>
         {person.biography
-          ? biographyTruncate(person.biography)
+          ? readMore
+            ? person.biography
+            : biographyTruncate(person.biography)
           : `We don't have a biography for ${person.name}`}
       </div>
       <div className="people-detail-right-header-modal-button">
         {person.biography ? (
           person.biography.length > 600 ? (
-            <button>MODAL BUTTON</button>
+            readMore ? null : (
+              <button onClick={() => setReadMore(true)}>Read More</button>
+            )
           ) : null
         ) : null}
       </div>
