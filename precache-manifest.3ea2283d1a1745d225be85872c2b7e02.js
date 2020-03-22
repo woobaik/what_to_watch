444fb568e5ca38042bc8cf26bb4251cd5639a281
Jbaik <joungwoo.baik@gmail.com>
2020-03-21 17:00:24 -0700

    Updates

---
+++ precache-manifest.3ea2283d1a1745d225be85872c2b7e02.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7de7526e59a76188d53a08bfe01eab4",
+    "url": "/what_to_watch/index.html"
+  },
+  {
+    "revision": "6bb7caf764e846a8f993",
+    "url": "/what_to_watch/static/css/2.ef80fd5d.chunk.css"
+  },
+  {
+    "revision": "40e6ecadf377ff6e9fab",
+    "url": "/what_to_watch/static/css/main.bac34ec9.chunk.css"
+  },
+  {
+    "revision": "6bb7caf764e846a8f993",
+    "url": "/what_to_watch/static/js/2.3fdc643c.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "/what_to_watch/static/js/2.3fdc643c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e6ecadf377ff6e9fab",
+    "url": "/what_to_watch/static/js/main.7c82eb12.chunk.js"
+  },
+  {
+    "revision": "a5bc318f67679431e989",
+    "url": "/what_to_watch/static/js/runtime-main.ad715020.js"
+  },
+  {
+    "revision": "20bb0abaa3b9c79ecf033311c337b8c3",
+    "url": "/what_to_watch/static/media/Joungwoo Baik-Resume.20bb0aba.pdf"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/what_to_watch/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/what_to_watch/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/what_to_watch/static/media/revicons.2feb69cc.eot"
+  }
+]);
