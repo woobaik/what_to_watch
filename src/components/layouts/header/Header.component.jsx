d90b8787df89c351f56372c4dded8f92d5ec10a2
Jbaik <joungwoo.baik@gmail.com>
2020-06-23 15:02:15 -0700

    Change main header color and remove logo

--- src/components/layouts/header/Header.component.jsx
+++ src/components/layouts/header/Header.component.jsx
@@ -7,24 +7,20 @@ import HorizontalDivider from "../divider/HorizontalDivider.component.jsx"
 import MovieSearchBar from "../../movies/movieSearchBar/MovieSearchBar.component"
 
 const Header = () => {
-  return (
-    <Fragment>
-      <header className="Header">
-        <Link to="/" className="logo-and-text">
-          <img
-            src={`${process.env.PUBLIC_URL}/assets/images/logo.png`}
-            alt="logo"
-            className="logo"
-          />
-          <div className="logo-name">WHAT TO WATCH?</div>
-        </Link>
-        <div className="header-search"></div>
-      </header>
-      <MovieNavigation />
-      <MovieSearchBar />
-      <HorizontalDivider />
-    </Fragment>
-  )
+	return (
+		<Fragment>
+			<header className="Header">
+				<Link to="/" className="logo-and-text">
+					<div className="logo-name">WHAT TO WATCH?</div>
+					<div className="logo-pill"></div>
+				</Link>
+				<div className="header-search"></div>
+			</header>
+			<MovieNavigation />
+			<MovieSearchBar />
+			<HorizontalDivider />
+		</Fragment>
+	)
 }
 
 export default Header
