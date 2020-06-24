4d82687938c88de9145d0049c193afcb965e10f0
Jbaik <joungwoo.baik@gmail.com>
2020-06-23 18:03:10 -0700

    Change styles after changing header

--- src/components/layouts/header/Header.component.jsx
+++ src/components/layouts/header/Header.component.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, Fragment } from "react"
 import { Link } from "react-router-dom"
 import "./Header.styles.scss"
 
 import MovieNavigation from "../../movies/movieNavBar/MovieNavigationBar.component"
-import HorizontalDivider from "../divider/HorizontalDivider.component.jsx"
+
 import MovieSearchBar from "../../movies/movieSearchBar/MovieSearchBar.component"
 
 const Header = () => {
@@ -18,21 +18,27 @@ const Header = () => {
 
 	useEffect(() => {
 		document.addEventListener("scroll", handleScroll)
+
+		return () => document.removeEventListener("scroll", handleScroll)
 	})
 
 	return (
-		<div className={visible ? "visible" : "visible-hidden"}>
-			<header className="Header">
-				<Link to="/" className="logo-and-text">
-					<div className="logo-name">WHAT TO WATCH?</div>
-					<div className="logo-pill"></div>
-				</Link>
-				<div className="header-search"></div>
-			</header>
-			<MovieNavigation />
+		<Fragment>
+			<div className={visible ? "visible" : "visible hidden"}>
+				<header className="Header">
+					<div className="header-left">
+						<Link to="/" className="logo-and-text">
+							<div className="logo-name">WHAT TO WATCH?</div>
+							<div className="logo-pill"></div>
+						</Link>
+						<div className="header-search"></div>
+					</div>
+
+					<MovieNavigation />
+				</header>
+			</div>
 			<MovieSearchBar />
-			<HorizontalDivider />
-		</div>
+		</Fragment>
 	)
 }
 
--- src/components/movies/movieNavBar/MovieNavigationBar.component.jsx
+++ src/components/movies/movieNavBar/MovieNavigationBar.component.jsx
@@ -1,61 +1,67 @@
 import React from "react"
 import "./MovieNavigationBar.style.scss"
 
+import { FiSearch } from "react-icons/fi"
 import { Link } from "react-router-dom"
 
+import MovieSearchBar from "../movieSearchBar/MovieSearchBar.component"
+
 const MovieNavigationBar = () => {
-  return (
-    <div className="Movie-navigation">
-      <div className="dropdown-container">
-        <Link className="nav-button" to="/movies" role="span">
-          MOVIES
-        </Link>
-        <div className="dropdown-hidden-container">
-          <Link to={"/movies"} className="dropdown-hidden-item">
-            Popular
-          </Link>
-          <Link to={"/movies/top-rated"} className="dropdown-hidden-item">
-            Top Rated
-          </Link>
-          <Link to={"/movies/upcoming"} className="dropdown-hidden-item">
-            Upcoming
-          </Link>
-          <Link to={"/movies/now-playing"} className="dropdown-hidden-item">
-            Now Playing
-          </Link>
-        </div>
-      </div>
-      <div className="dropdown-container">
-        <Link className="nav-button" to="/tvs" role="span">
-          TV SHOWS
-        </Link>
-        <div className="dropdown-hidden-container">
-          <Link to={"/tvs"} className="dropdown-hidden-item">
-            Popular
-          </Link>
-          <Link to={"/tvs/top-rated"} className="dropdown-hidden-item">
-            Top Rated
-          </Link>
-          <Link to={"/tvs/on-the-air"} className="dropdown-hidden-item">
-            On TV
-          </Link>
-          <Link to={"/tvs/airing-today"} className="dropdown-hidden-item">
-            Airing Today
-          </Link>
-        </div>
-      </div>
-      <div className="dropdown-container">
-        <Link className="nav-button" to="/people" role="span">
-          People
-        </Link>
-        <div className="dropdown-hidden-container">
-          <Link to={"/people"} className="dropdown-hidden-item">
-            Popular People
-          </Link>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div className="Movie-navigation">
+			<div className="dropdown-container">
+				<Link className="nav-button" to="/movies" role="span">
+					MOVIES
+				</Link>
+				<div className="dropdown-hidden-container">
+					<Link to={"/movies"} className="dropdown-hidden-item">
+						Popular
+					</Link>
+					<Link to={"/movies/top-rated"} className="dropdown-hidden-item">
+						Top Rated
+					</Link>
+					<Link to={"/movies/upcoming"} className="dropdown-hidden-item">
+						Upcoming
+					</Link>
+					<Link to={"/movies/now-playing"} className="dropdown-hidden-item">
+						Now Playing
+					</Link>
+				</div>
+			</div>
+			<div className="dropdown-container">
+				<Link className="nav-button" to="/tvs" role="span">
+					TV SHOWS
+				</Link>
+				<div className="dropdown-hidden-container">
+					<Link to={"/tvs"} className="dropdown-hidden-item">
+						Popular
+					</Link>
+					<Link to={"/tvs/top-rated"} className="dropdown-hidden-item">
+						Top Rated
+					</Link>
+					<Link to={"/tvs/on-the-air"} className="dropdown-hidden-item">
+						On TV
+					</Link>
+					<Link to={"/tvs/airing-today"} className="dropdown-hidden-item">
+						Airing Today
+					</Link>
+				</div>
+			</div>
+			<div className="dropdown-container">
+				<Link className="nav-button" to="/people" role="span">
+					People
+				</Link>
+				<div className="dropdown-hidden-container">
+					<Link to={"/people"} className="dropdown-hidden-item">
+						Popular People
+					</Link>
+				</div>
+			</div>
+			<div className="dropdown-container">
+				<FiSearch className="nav-button" />
+			</div>
+		</div>
+	)
 }
 
 export default MovieNavigationBar
