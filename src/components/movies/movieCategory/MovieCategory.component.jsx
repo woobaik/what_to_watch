a51e63d602dd3bc0da3075d9f1c8ad460f8bb457
Jbaik <joungwoo.baik@gmail.com>
2020-06-23 22:19:44 -0700

    change category header design

--- src/components/movies/movieCategory/MovieCategory.component.jsx
+++ src/components/movies/movieCategory/MovieCategory.component.jsx
@@ -4,37 +4,39 @@ import { connect } from "react-redux"
 
 import MovieCard from "../movieCard/MovieCard.components"
 
-const videoCoverList = videoList => {
-  const limitVideo4 = videoList.slice(0, 6)
-  return limitVideo4
+const videoCoverList = (videoList) => {
+	const limitVideo4 = videoList.slice(0, 6)
+	return limitVideo4
 }
 
-const MovieCategories = props => {
-  let videoList = []
-  if (props.categoryName === "On TV") {
-    videoList = videoCoverList(props.tvList.tvList)
-  } else {
-    videoList = videoCoverList(props.movieList.movies)
-  }
+const MovieCategories = (props) => {
+	let videoList = []
+	if (props.categoryName === "On TV") {
+		videoList = videoCoverList(props.tvList.tvList)
+	} else {
+		videoList = videoCoverList(props.movieList.movies)
+	}
 
-  return (
-    <div className="category-card-deck">
-      <div className="category-title">{props.categoryName}</div>
+	return (
+		<div className="category-card-deck">
+			<div className="category-title">
+				<span>{props.categoryName}</span>
+			</div>
 
-      <div className="moive-category">
-        {videoList.map(video => {
-          return <MovieCard key={video.id} video={video} />
-        })}
-      </div>
-    </div>
-  )
+			<div className="moive-category">
+				{videoList.map((video) => {
+					return <MovieCard key={video.id} video={video} />
+				})}
+			</div>
+		</div>
+	)
 }
 
-const mapStateToProps = state => {
-  return {
-    movieList: state.movies,
-    tvList: state.tvs
-  }
+const mapStateToProps = (state) => {
+	return {
+		movieList: state.movies,
+		tvList: state.tvs,
+	}
 }
 
 export default connect(mapStateToProps)(MovieCategories)
