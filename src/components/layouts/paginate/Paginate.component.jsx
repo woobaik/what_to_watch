7f38cc9cbd08d23ccb43681664d2ceb05790ccf4
Jbaik <joungwoo.baik@gmail.com>
2020-06-25 17:24:56 -0700

    Change movie detail page style in mobile devise

--- src/components/layouts/paginate/Paginate.component.jsx
+++ src/components/layouts/paginate/Paginate.component.jsx
@@ -1,25 +1,25 @@
 import React from "react"
 import "./Paginate.style.scss"
 import ReactPaginate from "react-paginate"
-const Paginate = props => {
-  return (
-    <div className="Paginate">
-      <ReactPaginate
-        previousLabel={"Previous"}
-        nextLabel={"Next"}
-        breakLabel={"..."}
-        breakClassName={"break-me"}
-        pageCount={500}
-        marginPagesDisplayed={1}
-        pageRangeDisplayed={5}
-        onPageChange={e => props.pageChange(e.selected)}
-        containerClassName={"pagination"}
-        subContainerClassName={"pages pagination"}
-        activeClassName={"active"}
-        forcePage={props.currentPage}
-      />
-    </div>
-  )
+const Paginate = (props) => {
+	return (
+		<div className="Paginate">
+			<ReactPaginate
+				previousLabel={"Previous"}
+				nextLabel={"Next"}
+				breakLabel={"..."}
+				breakClassName={"break-me"}
+				pageCount={500}
+				marginPagesDisplayed={1}
+				pageRangeDisplayed={2}
+				onPageChange={(e) => props.pageChange(e.selected)}
+				containerClassName={"pagination"}
+				subContainerClassName={"pages pagination"}
+				activeClassName={"active"}
+				forcePage={props.currentPage}
+			/>
+		</div>
+	)
 }
 
 export default Paginate
